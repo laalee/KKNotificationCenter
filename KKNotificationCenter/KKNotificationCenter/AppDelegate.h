3cc34c8e0cd81a22d01b2c7bc362b90334c23eb1
annieli <annieli@kkbox.com>
2019-01-30 16:09:18 +0800

    Create new project.

---
+++ KKNotificationCenter/KKNotificationCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKNotificationCenter
+//
+//  Created by Annie Lee on 2019/1/30.
+//  Copyright © 2019 Annie Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKNotificationCenter/KKNotificationCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKNotificationCenter
+//
+//  Created by Annie Lee on 2019/1/30.
+//  Copyright © 2019 Annie Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
